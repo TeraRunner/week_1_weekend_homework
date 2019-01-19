86d03a19405f7e7578218c75d0432bc51c85811c
Ruben Franco <rubenfrancosanchez@hotmail.es>
2019-01-19 17:56:11 +0000

    3rd optional test completed

--- pet_shop.rb
+++ pet_shop.rb
@@ -76,3 +76,10 @@ def customer_can_afford_pet(customer, pet)
     return true
   end
 end
+
+def sell_pet_to_customer(shop, pet, customer)
+    add_pet_to_customer(customer, pet)
+    shop[:admin][:pets_sold] += 1
+    customer[:cash] -= pet[:price]
+    shop[:admin][:total_cash] += pet[:price]
+end
