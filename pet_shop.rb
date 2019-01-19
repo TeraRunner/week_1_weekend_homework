959ba9eea24d920fbd0e9d6e5c35ffddcb81bde5
Ruben Franco <rubenfrancosanchez@hotmail.es>
2019-01-19 17:14:57 +0000

    1st optional test completed

--- pet_shop.rb
+++ pet_shop.rb
@@ -68,3 +68,9 @@ end
 def add_pet_to_customer(customer, pet)
   return customer[:pets] << pet
 end
+
+def customer_can_afford_pet(customer, pet)
+  if customer[:cash] < pet[:price]
+    return false
+  end
+end
